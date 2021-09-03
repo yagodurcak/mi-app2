fb3b4981d610eca45f540f222b90047b5978c930
yagodurcak <mauridurcak@gmail.com>
2021-09-02 21:26:02 -0300

    2323

--- miapp2/src/components/CartConteiner.js
+++ miapp2/src/components/CartConteiner.js
@@ -1,38 +1,39 @@
 import React,{ useContext }  from 'react';
 import ThemeContext from '../context/cartContext';
 import CardCart from './CardCart';
+import { Link } from 'react-router-dom';
 
 
 const CartContainer = () => {
 
-
-
    const {setCarrito,carrito} = useContext(ThemeContext);
-
+    
 
    const borrarTodo = () => {
        setCarrito([])
-   }
-   
-
-   
- 
-
-
-    // console.log(carrito);
+   };
 
     return (
-
         <div className="container">
             <div className="row">
                 <div className="col-12">
-                { carrito.map((pepe)=>
-                <CardCart nombre={pepe.nombre} precio={pepe.precio} cantidad={pepe.cantidad} id={pepe.id} />)}
+                    {(carrito.length == 0) ? <div>
+                        
+                            <h2 className="text-center mt-5">NO HAY PRODUCTOS EN EL CARRITO</h2>
+                            <Link to="/catalogo" className= "d-flex justify-content-center mt-5"> <button className="btn btn-primary">Volver a Catalogo</button></Link>
+                    </div>
+                        
+                     : 
+                         <div>
+                             {carrito.map(pepe => ( <CardCart nombre={pepe.nombre} precio={pepe.precio} cantidad={pepe.cantidad} id={pepe.id} />))}
+                             <button className="btn btn-danger mt-4" onClick={()=>borrarTodo()}> Eliminar todos</button>
+                         </div>
+                            }
+                    
                 </div>
-                <button className="btn btn-danger mt-4" onClick={()=>borrarTodo()}>Eliminar todos</button>
             </div>
-        </div>
-      );
-}
- 
+            </div>)
+       
+};
+
 export default CartContainer;
